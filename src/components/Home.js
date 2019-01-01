700acce3ecc69bed234c8369b3a0ed64167e9358
wangyishi <wangyishistl@gmail.com>
2019-01-01 02:03:49 -0600

    set up react-gird-gallery api and display posts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Tabs, Button, Spin } from 'antd';
 import {GEO_OPTIONS, POS_KEY, API_ROOT, TOKEN_KEY, AUTH_HEADER} from '../constants';
+import { Gallery } from './Gallery';
 
 const TabPane = Tabs.TabPane;
 
@@ -62,13 +63,27 @@ export class Home extends React.Component {
 
 
   getImagePosts = () => {
-    const {error, isLoadingGeoLocation } = this.state;
+    const { error, isLoadingGeoLocation, isLoadingPosts, posts } = this.state;
     if (error) {
       return <div>{error}</div>;
     } else if (isLoadingGeoLocation) {
       return <Spin tip="Loading geo location..."/>;
+    } else if (isLoadingPosts) {
+      return <Spin tip="Loading posts..."/>;
+    } else if (posts.length > 0) {
+      const images = this.state.posts.map((post) => {
+        return {
+          user: post.user,
+          src: post.url,
+          thumbnail: post.url,
+          caption: post.message,
+          thumbnailWidth: 400,
+          thumbnailHeight: 300,
+        }
+      });
+      return (<Gallery images={images}/>);
     } else {
-      return 'content of tab 1';
+      return 'No nearby posts.';
     }
   }
 
