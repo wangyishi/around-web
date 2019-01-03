a4d0c4041114cbf6beb240d819af52997d03ede6
wangyishi <wangyishistl@gmail.com>
2019-01-03 01:48:38 -0600

    onDragEnd

--- src/components/AroundMap.js
+++ src/components/AroundMap.js
@@ -9,12 +9,29 @@ import {POS_KEY} from '../constants'
 
 class NormalAroundMap extends React.Component {
 
+  reloadMarkers =() => {
+    const center = this.getCenter();
+    this.props.loadNearbyPosts(center);
+  }
+
+  getMapRef = (mapInstance) => {
+    this.map = mapInstance;
+  }
+
+
+  getCenter = () => {
+    const center = this.map.getCenter();
+    return {lat: center.lat(), lon: center.lng()}
+  }
+
   render() {
     const { lat, lon : lng} = JSON.parse(localStorage.getItem(POS_KEY));
     return (
       <GoogleMap
+        ref={this.getMapRef}
         defaultZoom={11}
         defaultCenter={{ lat, lng}}
+        onDragEnd={this.reloadMarkers}
       >
         {this.props.posts.map((post) => <AroundMarker post={post} key={post.url}/>)}
       </GoogleMap>
