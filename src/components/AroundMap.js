408058eb62772fd6252fa045c599d56a1feeb0b9
wangyishi <wangyishistl@gmail.com>
2019-01-03 02:00:40 -0600

    onZoomChanged

--- src/components/AroundMap.js
+++ src/components/AroundMap.js
@@ -1,27 +1,33 @@
-import React from 'react';
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-} from "react-google-maps";
-import {AroundMarker} from './AroundMarker'
-import {POS_KEY} from '../constants'
+import React from 'react'
+import { GoogleMap, withGoogleMap, withScriptjs } from 'react-google-maps';
+import { AroundMarker } from './AroundMarker';
+import { POS_KEY } from '../constants';
 
 class NormalAroundMap extends React.Component {
 
   reloadMarkers =() => {
     const center = this.getCenter();
-    this.props.loadNearbyPosts(center);
+    const radius = this.getRadius();
+    this.props.loadNearbyPosts(center, radius);
   }
 
   getMapRef = (mapInstance) => {
     this.map = mapInstance;
   }
 
-
   getCenter = () => {
     const center = this.map.getCenter();
     return {lat: center.lat(), lon: center.lng()}
+  }
+
+  getRadius = () => {
+    const center = this.map.getCenter();
+    const bounds = this.map.getBounds();
+    if (center && bounds) {
+      const ne = bounds.getNorthEast();
+      const right = new window.google.maps.LatLng(center.lat(), ne.lng());
+      return 0.001 * window.google.maps.geometry.spherical.computeDistanceBetween(center, right);
+    }
   }
 
   render() {
@@ -32,6 +38,7 @@ class NormalAroundMap extends React.Component {
         defaultZoom={11}
         defaultCenter={{ lat, lng}}
         onDragEnd={this.reloadMarkers}
+        onZoomChanged={this.reloadMarkers}
       >
         {this.props.posts.map((post) => <AroundMarker post={post} key={post.url}/>)}
       </GoogleMap>
