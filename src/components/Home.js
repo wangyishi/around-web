31c085cbfd9a99d5d2f68ce47e06f38e6ec6a5a5
wangyishi <wangyishistl@gmail.com>
2019-01-05 01:05:15 -0600

    faces around the world

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { Tabs, Spin, Row, Col } from 'antd';
-import {GEO_OPTIONS, POS_KEY, API_ROOT, TOKEN_KEY, AUTH_HEADER} from '../constants';
+import { Tabs, Spin, Row, Col, Radio } from 'antd';
+import { GEO_OPTIONS, POS_KEY, API_ROOT, AUTH_HEADER, TOKEN_KEY } from '../constants';
 import { Gallery } from './Gallery';
-import { CreatePostButton} from './CreatePostButton';
-import {AroundMap} from './AroundMap'
+import { CreatePostButton } from './CreatePostButton';
+import { AroundMap } from './AroundMap';
 
 const TabPane = Tabs.TabPane;
+const RadioGroup = Radio.Group;
 
 export class Home extends React.Component {
-
   state = {
     isLoadingGeoLocation: false,
     error: '',
     isLoadingPosts: false,
     posts: [],
+    topic: 'around',
   }
 
   componentDidMount() {
@@ -37,7 +38,6 @@ export class Home extends React.Component {
   }
 
   onFailedLoadGeoLocation = () => {
-    console.log("Failed to load geo location");
     this.setState({ isLoadingGeoLocation: false, error: 'Failed to load geolocation.' });
   }
 
@@ -46,7 +46,7 @@ export class Home extends React.Component {
     const range = radius ? radius : 20;
     const token = localStorage.getItem(TOKEN_KEY);
     this.setState({ isLoadingPosts: true, error: '' });
-    return  fetch(`${API_ROOT}/search?lat=${lat}&lon=${lon}&range=${range}`, {
+    return fetch(`${API_ROOT}/search?lat=${lat}&lon=${lon}&range=${range}`, {
       method: 'GET',
       headers: {
         Authorization: `${AUTH_HEADER} ${token}`,
@@ -65,15 +65,14 @@ export class Home extends React.Component {
     });
   }
 
-
   getPanelContent = (type) => {
     const { error, isLoadingGeoLocation, isLoadingPosts, posts } = this.state;
     if (error) {
-      return <div>{error}</div>;
-    } else if (isLoadingGeoLocation) {
-      return <Spin tip="Loading geo location..."/>;
+      return <div>{error}</div>
+    } else if(isLoadingGeoLocation) {
+      return <Spin tip="Loading geo location..."/>
     } else if (isLoadingPosts) {
-      return <Spin tip="Loading posts..."/>;
+      return <Spin tip="Loading posts..." />
     } else if (posts.length > 0) {
       return type === 'image' ? this.getImagePosts() : this.getVideoPosts();
     } else {
@@ -86,8 +85,8 @@ export class Home extends React.Component {
       <Row gutter={32}>
         {this.state.posts.filter((post) => post.type === 'video').map((post) => {
           return (
-            <Col key={post.url} span={6} >
-              <video src={post.url} controls className='video-block'/>
+            <Col span={6} key={post.url}>
+              <video src={post.url} controls className="video-block"/>
               <p>{post.user}: {post.message}</p>
             </Col>
           );
@@ -109,27 +108,73 @@ export class Home extends React.Component {
           thumbnailHeight: 300,
         }
       });
+
     return (<Gallery images={images}/>);
+  }
+
+  onTopicChange = (e) => {
+    const topic = e.target.value;
+    this.setState({ topic });
+    if (topic === 'around') {
+      this.loadNearbyPosts();
+    } else {
+      this.loadFacesAroundTheWorld();
+    }
+  }
+
+  loadFacesAroundTheWorld = () => {
+    const token = localStorage.getItem(TOKEN_KEY);
+    this.setState({ isLoadingPosts: true, error: '' });
+    fetch(`${API_ROOT}/cluster?term=face`, {
+      method: 'GET',
+      headers: {
+        Authorization: `${AUTH_HEADER} ${token}`,
+      },
+    }).then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+      .then((data) => {
+        console.log(data);
+        this.setState({ isLoadingPosts: false, posts: data ? data : [] });
+      })
+      .catch((e) => {
+        console.log(e);
+        this.setState({ isLoadingPosts: false, error: 'Loading face images failed.'});
+      });
   }
 
   render() {
     const operations = <CreatePostButton loadNearbyPosts={this.loadNearbyPosts}/>;
     return (
-      <Tabs tabBarExtraContent={operations} className="main-tabs">
-        <TabPane tab="Image Posts" key="1">{this.getPanelContent('image')}</TabPane>
-        <TabPane tab="Video Posts" key="2">{this.getPanelContent('video')}</TabPane>
-        <TabPane tab="Map" key="3">
-          <AroundMap
-            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyD3CEh9DXuyjozqptVB5LA-dN7MxWWkr9s&v=3.exp&libraries=geometry,drawing,places"
-            loadingElement={<div style={{ height: `100%` }} />}
-            containerElement={<div style={{ height: `800px` }} />}
-            mapElement={<div style={{ height: `100%` }} />}
-            posts={this.state.posts}
-            loadNearbyPosts={this.loadNearbyPosts}
-          />
-        </TabPane>
-      </Tabs>
+      <div className="home">
+        <RadioGroup onChange={this.onTopicChange} value={this.state.topic} className="topic-radio-group">
+          <Radio value="around">Posts Around Me</Radio>
+          <Radio value="face">Faces Around The World</Radio>
+        </RadioGroup>
+        <Tabs tabBarExtraContent={operations} className="main-tabs">
+          <TabPane tab="Image Posts" key="1">
+            {this.getPanelContent('image')}
+          </TabPane>
+          <TabPane tab="Video Posts" key="2">
+            {this.getPanelContent('video')}
+          </TabPane>
+          <TabPane tab="Map" key="3">
+            <AroundMap
+              googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyD3CEh9DXuyjozqptVB5LA-dN7MxWWkr9s&v=3.exp&libraries=geometry,drawing,places"
+              loadingElement={<div style={{ height: `100%` }} />}
+              containerElement={<div style={{ height: `800px` }} />}
+              mapElement={<div style={{ height: `100%` }} />}
+              posts={this.state.posts}
+              loadNearbyPosts={this.loadNearbyPosts}
+              loadFacesAroundTheWorld={this.loadFacesAroundTheWorld}
+              topic={this.state.topic}
+            />
+          </TabPane>
+        </Tabs>
+      </div>
     );
   }
-
 }
